33cbb0bb6f22e4a5b7a3a2a906c7e60a8abdb167
Michael Weber <msweber@gmail.com>
2013-12-30 19:12:48 +1100

    Added watcher delays.

--- SubredditWatcherHandler.py
+++ SubredditWatcherHandler.py
@@ -1,14 +1,21 @@
 from SubredditWatcher import SubredditWatcher
+import time
 
 class SubredditWatcherHandler:
+
+    DELAY = 2 # seconds
 
     def __init__(self, praw_reddit, username, triggers, subreddits):
         self.subreddits = subreddits
         self.triggers = triggers
         self.subreddit_watchers = []
+        self.last_run = None
         for subreddit_name in subreddits:
             self.subreddit_watchers.append(SubredditWatcher(subreddit_name, praw_reddit, triggers, username))
 
     def process_subreddits(self):
-        for watcher in self.subreddit_watchers:
-            watcher.watch()
+        now = int(time.time())
+        if self.last_run is None or now - self.last_run > self.DELAY:
+            for watcher in self.subreddit_watchers:
+                watcher.watch()
+            self.last_run = int(time.time())
