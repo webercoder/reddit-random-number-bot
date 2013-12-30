a83ff16bce3ff32e2e6a4e1d3a00e53af5ad12a0
Michael Weber <msweber@gmail.com>
2013-12-30 13:43:56 +1100

    Abstracted message parsing code into MessageWatcher and broke down code across MessageParser and SubredditWatcher to be more testable and extendable.

---
+++ MessageParser.py
@@ -0,0 +1,67 @@
+import number
+import util
+from random import SystemRandom
+
+class MessageParser:
+    
+    def __init__(self, triggers):
+        self.triggers = triggers
+        self.username = username
+
+    def parse(self, msg):
+        successes = []
+        failures = []
+        msg = msg.lower()
+        msg_lines = msg.split("\n")
+        for line in msg_lines:
+            try:
+                successes.append(self.parse_line(line))
+            except e:
+                failures.append(str(e))
+        return {'successes':successes, 'failures':failures}
+
+    def parse_line(self, line):
+        username_mention = "/u/%s" % self.username;
+        if line.startswith(self.triggers) or line.startswith(username_mention):
+            x,y = extract_numbers(line)
+            randnum = self.so_random(x, y)
+            return {'x':x, 'y':y, 'random':randnum}
+
+    def extract_numbers(self, line):
+        try:
+            user,x,y = line.split()
+        except:
+            raise Exception("Unable to split the line into three parts for user, x, and y.")
+
+        if x is None or y is None: 
+            raise Exception("A lower and upper value are currently required.")
+
+        try:
+            if not x is None and x.lower() == "moon":
+                x = 384400 # distance to the moon in kms
+            if not y is None and y.lower() == "moon":
+                y = 384400 # distance to the moon in kms
+        except:
+            raise Exception("Unable to determine if x or y == 'moon'.")
+
+        xInt = self.num(x)
+        yInt = self.num(y)
+
+        if xInt > yInt:
+            tmpInt = xInt
+            xInt = yInt
+            yInt = tmpInt
+
+        return x,y
+
+
+    # Get an integer from a specified upper or lower value.
+    def num(val):
+        try:
+            return int(val)
+        except ValueError:
+            return 1
+
+    # Get a random number from the system. It must be within the specified bounds.
+    def so_random(x, y):
+        return SystemRandom().randint(x, y)
--- SubredditWatcher.py
+++ SubredditWatcher.py
@@ -13,66 +13,44 @@ class SubredditWatcher:
         self.praw_reddit = praw_reddit
         self.triggers = triggers
         self.already_done = []
+        self.msg_parser = MessageParser()
 
     def watch(self):
         subreddit = self.praw_reddit.get_subreddit(self.name)
         util.bot_stdout_print("Getting comments for subreddit: %s" % (self.name))
         for submission in subreddit.get_comments():
-            current_id = 0
-            try:
-                if submission.id not in self.already_done:
-                    comment_text = submission.body.lower()
-                    comment_text_lines = comment_text.split("\n")
-                    results = []
-                    for line in comment_text_lines:
-                        if line.startswith(self.triggers):
-                            current_id = submission.id
-                            util.bot_stdout_print("Found line, '%s'" % line)
-                            try:
-                                user,x,y = line.split()
-                            except:
-                                self.already_done.append(submission.id)
-                                util.bot_stdout_print("Invalid syntax. Skipped.")
-                                self.reply_usage(submission)
-                                continue 
-                            if x is None or y is None: 
-                                self.already_done.append(submission.id)
-                                util.bot_stdout_print("Invalid syntax. Skipped.")
-                                self.reply_usage(submission)
-                                continue 
-                            util.bot_stdout_print("Found parts: %s, %s, %s" % (user, x, y))
-                            try:
-                                if not x is None and x.lower() == "moon":
-                                    x = 384400 # distance to the moon in kms
-                                if not y is None and y.lower() == "moon":
-                                    y = 384400 # distance to the moon in kms
-                            except:
-                                util.bot_stdout_print("Error finding moon: %s" % sys.exc_info()[0])
-                            util.bot_stdout_print("x, y = %s, %s" % (str(x), str(y)))
-                            xInt = number.num(x)
-                            yInt = number.num(y)
-                            if xInt > yInt:
-                                tmpInt = xInt
-                                xInt = yInt
-                                yInt = tmpInt
-                            randnum = number.so_random(xInt, yInt)
-                            reply = "Random Number between %d and %d is %d." % (xInt, yInt, randnum)
-                            util.bot_stdout_print(reply)
-                            results.append(reply)
-                            self.already_done.append(submission.id)
-                            continue
-                    if len(results) > 0:
-                        reply = "   \n".join(results)
-                        results = []
-                        util.bot_stdout_print("Posting comment for %s:\n%s" % (submission.id, reply))
+            if submission.id not in self.already_done:
+                try:
+                    results = self.msg_parser(submission.body)
+                    self.already_done.append(submission.id)
+                    reply = self.get_reply(submission, results)
+                    if not reply is None:
                         reddit.handle_ratelimit(submission.reply, reply)
-            except:
-                util.bot_stdout_print("Unknown exception: %s" % sys.exc_info()[0])
-                print traceback.format_exc()
-                self.already_done.append(current_id)
-                self.reply_usage(submission)
-                continue
+                except:
+                    util.bot_stdout_print("Unknown exception: %s" % sys.exc_info()[0])
+                    print traceback.format_exc()
+                    self.already_done.append(current_id)
         self.cleanup_already_done()
+
+    def get_reply(self, results):
+
+        if len(results.successes) > 0:
+            sucess_msgs = []
+            for result in results.successes:
+                sucess_msgs.append("Random integer between %d and %d is %d." % (result.x, result.y, result.random))
+            reply = "  \n".join(failure_messages)
+            return reply
+
+        elif len(results.failures) > 0:
+            failure_messages = []
+            for failure_msg in results.failures:
+                failure_messages.append("* %s" % failure_msg)
+            reply = "The following errors occurred:  \n  \n"
+            reply += "  \n".join(failure_messages)
+            reply += "  \n  \nYou may be doing something incorrectly."
+            reply += " Please enter the following command to use this bot: "
+            reply += "\"/u/%s x y\" (where x and y are integers)." % self.username
+            return reply
 
     def cleanup_already_done(self):
         # Thought about just removing everything that is not in the current reddit comment list, 
@@ -82,8 +60,3 @@ class SubredditWatcher:
         if len(self.already_done) > self.MAX_ALREADY_DONE_LENGTH:
             negative_length = -1 * self.MAX_ALREADY_DONE_LENGTH
             self.already_done = self.already_done[negative_length:]
-
-    # Post a reddit comment about proper usage when someone uses this bot incorrectly.
-    def reply_usage(self, submission):
-        reply = "You may be doing something incorrectly. Please enter the following command to use this bot: \"%s x y\" (where x and y are integers)." % self.triggers[0]
-        reddit.handle_ratelimit(submission.reply, reply)
