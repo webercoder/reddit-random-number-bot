e0127ce09240b7f774835412624732c672a3bfd3
Michael Weber <msweber@gmail.com>
2013-12-30 13:58:00 +1100

    MentionsWatcher now has an already_seen list and it loops through mentions. Untested, not complete.

--- MentionsWatcher.py
+++ MentionsWatcher.py
@@ -5,7 +5,9 @@ class MentionsWatcher:
     def __init__(self, praw_reddit):
         self.praw_reddit = praw_reddit
         self.username = username
+        self.already_seen = []
 
     def watch(self):
         mentions = self.praw_reddit.get_mentions()
-        
+        for mention in mentions:
+            print mention
